6e917968e0c32ad905b4fcf88700ada552dfdebc
Jessé Moraes Braga <jesse.braga@iits-consulting.de>
2022-12-16 02:19:34 +0100

    Day 07

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,20 +1,25 @@
 private const val MAX_FILE_SIZE = 100_000
+private const val TOTAL_DISK_SIZE = 70_000_000
+private const val MIN_FREE_SPACE_FOR_UPDATE = 30_000_000
 
-data class DirectoryOrFile(val name: String, val size: Long?, val parentDirectory: String?, val subdirectories: List<DirectoryOrFile>?, val files: List<Long>?) {
-    fun getSumOfFileSizes(): Long = subdirectories?.let { subdirectories ->
-        subdirectories.sumOf { it.getSumOfFileSizes() }
-            .plus(files?.sum() ?: 0)
-    } ?: 0
-
+data class Directory(
+    val name: String,
+    val parentDirectory: String,
+    val subdirectories: MutableList<Directory> = mutableListOf(),
+    val files: MutableList<Long> = mutableListOf(),
+) {
+    fun getSumOfFileSizes(): Long = subdirectories.sumOf { directory ->
+        directory.getSumOfFileSizes()
+    }.plus(files.sum())
 }
 
 fun main() {
 
     val input = readInput("Day07Input").split("\n")
-    val fileTree = HashMap<String, MutableMap<Long, String>>()
-    var currentDirectory = "/"
+    val fileTree = HashMap<String, Directory>()
+    var currentDirectory = Directory("/", "/")
 
-    fileTree[currentDirectory] = mutableMapOf()
+    fileTree[currentDirectory.name] = currentDirectory
 
     for (line in input.subList(2, input.lastIndex)) {
         when (line.split(" ").first()) {
@@ -22,37 +27,51 @@ fun main() {
                 val command = line.removePrefix("$ ").split(" ")
                 when (command.first()) {
                     "cd" ->
-                        currentDirectory =
-                            (if (command.last() == "..") getParentDirectoryFor(currentDirectory) else getNewDirectoryName(
-                                currentDirectory,
-                                command
-                            ))
+                        currentDirectory = if (command.last() == "..") {
+                            fileTree[currentDirectory.parentDirectory] ?: error("no parent directory")
+                        } else {
+                            fileTree[determineDirectoryName(currentDirectory, command.last())]
+                                ?: error("directory ${command.last()} not found")
+                        }
 
                     "ls" -> continue
-                    else -> throw error("command unknown: <${command.first()}>")
                 }
             }
 
             "dir" -> {
-                val name = line.removePrefix("dir ")
-                val subDirectory = if (currentDirectory == "/") "$currentDirectory$name" else "$currentDirectory/$name"
-                fileTree[subDirectory] = mutableMapOf()
+                val subDirectory = Directory(
+                    name = determineDirectoryName(currentDirectory, line.removePrefix("dir ")),
+                    parentDirectory = currentDirectory.name,
+                )
+                currentDirectory.subdirectories.add(subDirectory)
+                fileTree[subDirectory.name] = subDirectory
             }
 
             else -> {
                 val fileInfo = line.split(" ")
-                fileTree[currentDirectory]!![fileInfo.first().toLong()] = fileInfo.last()
+                fileTree[currentDirectory.name]?.files?.add(fileInfo.first().toLong())
             }
         }
     }
 
     findDirectoriesWithLessThanMaxFileSize(fileTree).sum().also { println(it) }
+    val usedDiskSpace = fileTree["/"]!!.getSumOfFileSizes()
+    val freeDiskSpace = TOTAL_DISK_SIZE - usedDiskSpace // 26_496_260
+    val directorySizeWanted = MIN_FREE_SPACE_FOR_UPDATE - freeDiskSpace // 3_503_740
+    println(findDirectoryWithFileSize(directorySizeWanted, fileTree).getSumOfFileSizes())
 }
 
-fun findDirectoriesWithLessThanMaxFileSize(fileTree: java.util.HashMap<String, MutableMap<Long, String>>): MutableList<Long> {
+fun findDirectoryWithFileSize(fileSize: Long, fileTree: HashMap<String, Directory>) =
+    fileTree.values.filter {
+        it.getSumOfFileSizes() >= fileSize
+    }.minBy {
+        it.getSumOfFileSizes()
+    }
+
+fun findDirectoriesWithLessThanMaxFileSize(fileTree: HashMap<String, Directory>): MutableList<Long> {
     val directories = mutableListOf<Long>()
     fileTree.keys.forEach { key ->
-        val sum = fileTree[key]!!.keys.sum()
+        val sum = fileTree[key]!!.getSumOfFileSizes()
         if (sum <= MAX_FILE_SIZE) {
             directories.add(sum)
         }
@@ -60,13 +79,5 @@ fun findDirectoriesWithLessThanMaxFileSize(fileTree: java.util.HashMap<String, M
     return directories
 }
 
-private fun getNewDirectoryName(currentDirectory: String, command: List<String>) =
-    if (currentDirectory == "/") "/${command.last()}" else "$currentDirectory/${command.last()}"
-
-fun getParentDirectoryFor(currentDirectory: String): String =
-    currentDirectory
-        .split("/")
-        .drop(1)
-        .run {
-            return@run if (this.size == 1) "/" else "/${this.dropLast(1).joinToString("/")}"
-        }
+private fun determineDirectoryName(currentDirectory: Directory, name: String) =
+    if (currentDirectory.name == "/") "/$name" else "${currentDirectory.name}/$name"
